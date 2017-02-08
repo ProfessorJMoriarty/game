0fe628a5c37a53be3566a2059918ab4af941bc5f
DAVID_TORRES <DAVID_TORRES@605-113B-DTORR>
2017-02-08 07:42:22 -0700

    commit

---
+++ App1/App1/App.xaml.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream> { //Library }
+using namespace std; 
+
+int Main() { //Starting Point of Program}
+	int input;
+	cout << "How many Cookies would you like" << endl;
+	cin >> input;
+
+	if (input <= 5) {
+		cout << "Are you sure you want that quantity" << endl;
+	}
+	else if (input < 10)
+		cout << "Here take" << input << " Cookie(s)" << endl;
+	else
+		cout << "Be nice and leave some nice to others" << end;
+
+
+
+
---
+++ Cookies/Cookies/hola.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hellow mundo";
+	cin.get();
+	return 0;
+}
+
